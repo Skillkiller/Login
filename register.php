f89c84ff7b64a47e4e51cd3ef02b41ca8b9ff59a
Skillkiller <Skillkiller@users.noreply.github.com>
2016-07-22 02:14:18 +0200

    Fix gegen eine Registrierung ohne Daten

--- register.php
+++ register.php
@@ -48,35 +48,39 @@
         $pw = md5($_POST["password1"]);
         $pw2 = md5($_POST["password2"]);
         
-        if ($pw != $pw2) {
-            echo "Deine Passw&ouml;rter stimmen nicht &uuml;berein....<a href=\"register.php\">zur&uuml;ck</a>";
+        if (!empty($pw) or !empty($user)) {
+        	if ($pw != $pw2) {
+            		echo "Deine Passw&ouml;rter stimmen nicht &uuml;berein....<a href=\"register.php\">zur&uuml;ck</a>";
+        	} else {
+            		include(__DIR__ . "/config/Verbindungen.php"); 
+            		$control = 0;
+            		$abfrage = "SELECT username FROM user WHERE user = '$user'";
+            		$ergebnis = mysql_query($abfrage);
+            		while ($row = mysql_fetch_object($ergebnis))
+               		{
+                   		$control++;
+               		} 
+            		if ($control != 0) {
+                		echo "Dieser Name ist schon vergeben. <a href=\"register.php\">zur&uuml;ck</a>";
+            		} else {
+                		$eintrag = "INSERT INTO user
+                		(username,password)
+                
+                		VALUES
+                		('$user', '$pw')";
+                
+                		$eintragen = mysql_query($eintrag);
+                		if ($eintragen == true) {
+                    			echo "Regestrierung abgeschlossen.  <a href=\"index.php\">Jetzt anmelden</a>";
+                		} else {
+                    			echo "Fehler im System.";
+                		}
+                		mysql_close($verbindung);    
+            		}   
+        	}	
+        	
         } else {
-            include(__DIR__ . "/config/Verbindungen.php");
-                        
-            $control = 0;
-            $abfrage = "SELECT username FROM user WHERE user = '$user'";
-            $ergebnis = mysql_query($abfrage);
-            while ($row = mysql_fetch_object($ergebnis))
-               {
-                   $control++;
-               } 
-            if ($control != 0) {
-                echo "Dieser Name ist schon vergeben. <a href=\"register.php\">zur&uuml;ck</a>";
-            } else {
-                $eintrag = "INSERT INTO user
-                (username,password)
-                
-                VALUES
-                ('$user', '$pw')";
-                
-                $eintragen = mysql_query($eintrag);
-                if ($eintragen == true) {
-                    echo "Regestrierung abgeschlossen.  <a href=\"index.php\">Jetzt anmelden</a>";
-                } else {
-                    echo "Fehler im System.";
-                }
-                mysql_close($verbindung);    
-            }   
+        	echo "Du darfst kein Feld leer lassen! <a href=\"register.php\">zur&uuml;ck</a>"	
         }
     }
     ?>
