5e5a13f538d7bff78ca011048ae35b7d8a33ad16
Skillkiller <Skillkiller@users.noreply.github.com>
2016-08-27 00:21:52 +0200

    Update Verbindungen.php

--- config/Verbindungen.php
+++ config/Verbindungen.php
@@ -2,6 +2,6 @@
 $verbindung = mysqli_connect("localhost", "root", "samp")
 or die ("Fehler bei der Anmeldung");
 
-mysqli_select_db($verbindung, "db_seb")
+mysqli_select_db($verbindung, "Datenbank")
 or die ("Verbindung zur Datenbank gescheitert");
 ?>
